55e94f262f047a1ceb23f536ff4bb554147d854e
mbustamante <mbustamante@aseinfo.com.sv>
2017-01-25 11:34:35 -0600

    Added script watching

--- EvoScriptExecute/EvoScriptExecute/Form1.Designer.cs
+++ EvoScriptExecute/EvoScriptExecute/Form1.Designer.cs
@@ -39,11 +39,18 @@
             this.txtEvoConfig = new System.Windows.Forms.TextBox();
             this.label3 = new System.Windows.Forms.Label();
             this.txtEvoTemp = new System.Windows.Forms.TextBox();
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.tabControl1.SuspendLayout();
+            this.tabPage1.SuspendLayout();
+            this.tabPage2.SuspendLayout();
             this.SuspendLayout();
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(94, 12);
+            this.button1.Location = new System.Drawing.Point(107, 22);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(75, 23);
             this.button1.TabIndex = 0;
@@ -53,7 +60,7 @@
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(12, 72);
+            this.textBox1.Location = new System.Drawing.Point(25, 82);
             this.textBox1.Multiline = true;
             this.textBox1.Name = "textBox1";
             this.textBox1.ScrollBars = System.Windows.Forms.ScrollBars.Both;
@@ -62,14 +69,14 @@
             // 
             // textBox2
             // 
-            this.textBox2.Location = new System.Drawing.Point(280, 45);
+            this.textBox2.Location = new System.Drawing.Point(293, 55);
             this.textBox2.Name = "textBox2";
             this.textBox2.Size = new System.Drawing.Size(224, 20);
             this.textBox2.TabIndex = 2;
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(13, 12);
+            this.button2.Location = new System.Drawing.Point(26, 22);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(75, 23);
             this.button2.TabIndex = 3;
@@ -79,7 +86,7 @@
             // 
             // button3
             // 
-            this.button3.Location = new System.Drawing.Point(199, 43);
+            this.button3.Location = new System.Drawing.Point(212, 53);
             this.button3.Name = "button3";
             this.button3.Size = new System.Drawing.Size(75, 23);
             this.button3.TabIndex = 4;
@@ -90,7 +97,7 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(175, 17);
+            this.label1.Location = new System.Drawing.Point(185, 24);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(52, 13);
             this.label1.TabIndex = 5;
@@ -98,7 +105,7 @@
             // 
             // txtEvoData
             // 
-            this.txtEvoData.Location = new System.Drawing.Point(233, 14);
+            this.txtEvoData.Location = new System.Drawing.Point(246, 24);
             this.txtEvoData.Name = "txtEvoData";
             this.txtEvoData.Size = new System.Drawing.Size(100, 20);
             this.txtEvoData.TabIndex = 6;
@@ -107,7 +114,7 @@
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(339, 17);
+            this.label2.Location = new System.Drawing.Point(349, 24);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(59, 13);
             this.label2.TabIndex = 7;
@@ -115,7 +122,7 @@
             // 
             // txtEvoConfig
             // 
-            this.txtEvoConfig.Location = new System.Drawing.Point(404, 14);
+            this.txtEvoConfig.Location = new System.Drawing.Point(417, 24);
             this.txtEvoConfig.Name = "txtEvoConfig";
             this.txtEvoConfig.Size = new System.Drawing.Size(100, 20);
             this.txtEvoConfig.TabIndex = 8;
@@ -124,7 +131,7 @@
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(12, 48);
+            this.label3.Location = new System.Drawing.Point(22, 55);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(56, 13);
             this.label3.TabIndex = 9;
@@ -132,32 +139,75 @@
             // 
             // txtEvoTemp
             // 
-            this.txtEvoTemp.Location = new System.Drawing.Point(74, 45);
+            this.txtEvoTemp.Location = new System.Drawing.Point(87, 55);
             this.txtEvoTemp.Name = "txtEvoTemp";
             this.txtEvoTemp.Size = new System.Drawing.Size(100, 20);
             this.txtEvoTemp.TabIndex = 10;
             this.txtEvoTemp.Text = "EvoTemp";
             // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.tabPage1);
+            this.tabControl1.Controls.Add(this.tabPage2);
+            this.tabControl1.Location = new System.Drawing.Point(-5, -2);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(555, 333);
+            this.tabControl1.SizeMode = System.Windows.Forms.TabSizeMode.FillToRight;
+            this.tabControl1.TabIndex = 12;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.BackColor = System.Drawing.SystemColors.Control;
+            this.tabPage1.Controls.Add(this.button2);
+            this.tabPage1.Controls.Add(this.txtEvoTemp);
+            this.tabPage1.Controls.Add(this.button1);
+            this.tabPage1.Controls.Add(this.label3);
+            this.tabPage1.Controls.Add(this.textBox1);
+            this.tabPage1.Controls.Add(this.txtEvoConfig);
+            this.tabPage1.Controls.Add(this.textBox2);
+            this.tabPage1.Controls.Add(this.label2);
+            this.tabPage1.Controls.Add(this.button3);
+            this.tabPage1.Controls.Add(this.txtEvoData);
+            this.tabPage1.Controls.Add(this.label1);
+            this.tabPage1.Location = new System.Drawing.Point(4, 22);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Size = new System.Drawing.Size(547, 307);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "Ejecutar Scripts";
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.BackColor = System.Drawing.SystemColors.Control;
+            this.tabPage2.Controls.Add(this.comboBox1);
+            this.tabPage2.Location = new System.Drawing.Point(4, 22);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(547, 307);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "Scripts";
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(13, 6);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(516, 21);
+            this.comboBox1.TabIndex = 0;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(519, 287);
-            this.Controls.Add(this.txtEvoTemp);
-            this.Controls.Add(this.label3);
-            this.Controls.Add(this.txtEvoConfig);
-            this.Controls.Add(this.label2);
-            this.Controls.Add(this.txtEvoData);
-            this.Controls.Add(this.label1);
-            this.Controls.Add(this.button3);
-            this.Controls.Add(this.button2);
-            this.Controls.Add(this.textBox2);
-            this.Controls.Add(this.textBox1);
-            this.Controls.Add(this.button1);
+            this.ClientSize = new System.Drawing.Size(540, 325);
+            this.Controls.Add(this.tabControl1);
             this.Name = "Form1";
             this.Text = "Ejecución de Scripts";
+            this.tabControl1.ResumeLayout(false);
+            this.tabPage1.ResumeLayout(false);
+            this.tabPage1.PerformLayout();
+            this.tabPage2.ResumeLayout(false);
             this.ResumeLayout(false);
-            this.PerformLayout();
 
         }
 
@@ -174,6 +224,10 @@
         private System.Windows.Forms.TextBox txtEvoConfig;
         private System.Windows.Forms.Label label3;
         private System.Windows.Forms.TextBox txtEvoTemp;
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.ComboBox comboBox1;
     }
 }
 
